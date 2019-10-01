5c3c23670544621a424f559fd0c5580c0854b70c
ndesell <ndesell@gmail.com>
2019-09-30 22:49:03 -0400

    submission 1

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by NTD on 9/29/19.
+//  Copyright © 2019 Nelson Desello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
